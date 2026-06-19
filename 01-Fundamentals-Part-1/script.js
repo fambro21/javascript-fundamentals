001bedf8445c52d2f0f6bde239398931009ee4c6
jonathan_fambro <startek591@gmail.com>
2026-06-19 12:30:51 -0400

    feat:add examples of variable naming conventions

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -8,4 +8,17 @@
   let value3 = 23;
 
   let firstName = "Matilda";
+
+  // Variable name conventions
+  let jonas_matilda = "JM";
+  let $function = 27;
+
+  let person = "jonas";
+  let PI = 3.1415;
+
+  let myFirstJob = "Coder";
+  let myCurrentJob = "Teacher";
+
+  let job1 = "programmer";
+  let job2 = "teacher";
 })();
