b4448ae37125d71c6606e41c5a2c4a8fd1fdc53f
jonathan_fambro <startek591@gmail.com>
2026-06-19 12:08:30 -0400

    feat:add variables and values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -2,4 +2,10 @@
   // Linking a JavaScript File
   let js = "amazing";
   let value1 = 40 + 8 + 23 - 10;
+
+  // Values and Variables
+  let value2 = "Jonas";
+  let value3 = 23;
+
+  let firstName = "Matilda";
 })();
