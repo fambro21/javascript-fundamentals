4864ca9f39405978bd9d6c79a3f1899f57ade029
jonathan_fambro <startek591@gmail.com>
2026-06-23 16:55:13 -0400

    feat:add example of maps and maps fundamentals

--- 04-Data-Structures-Operators/script.js
+++ 04-Data-Structures-Operators/script.js
@@ -205,4 +205,51 @@
   planesInLine(5);
   planesInLine(3);
   planesInLine(12);
+
+  // Maps: Iteration
+  const question = new Map([
+    ["question", "What is the best programming language in the world?"],
+    [1, "C"],
+    [2, "Java"],
+    [3, "JavaScript"],
+    ["correct", 3],
+    [true, "Correct 🎉"],
+    [false, "Try again!"],
+  ]);
+  console.log(question);
+
+  // Convert object to map
+  console.log(Object.entries(openingHours));
+  const hoursMap = new Map(Object.entries(openingHours));
+  console.log(hoursMap);
+
+  // Maps: Fundamentals
+  const rest = new Map();
+  rest.set("name", "Classico Italiano");
+  rest.set(1, "Firenze, Italy");
+  console.log(rest.set(2, "Listbon, Portugal"));
+
+  rest
+    .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+    .set("open", 11)
+    .set("close", 23)
+    .set(true, "We are open :D")
+    .set(false, "We are clsed :(");
+
+  console.log(rest.get("name"));
+  console.log(rest.get(true));
+  console.log(rest.get(1));
+
+  const time = 8;
+  console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+  console.log(rest.has("categories"));
+  rest.delete(2);
+  rest.clear();
+
+  const arr = [1, 2];
+  rest.set(arr, "Test");
+  console.log(rest);
+  console.log(rest.size);
+
+  console.log(rest.get(arr));
 })();
