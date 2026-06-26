4b4c28f5e8f7a78a51e738f7cf3abbadea0ab519
jonathan_fambro <startek591@gmail.com>
2026-06-26 15:38:51 -0400

    feat:add examples of assignment, nullish, and short circuiting

--- 04-Data-Structures-Operators/script.js
+++ 04-Data-Structures-Operators/script.js
@@ -360,4 +360,56 @@
   }
 
   console.log([...menu.entries()]);
+
+  // Logical Assignment Operators
+  const rest1 = {
+    name: "Capri",
+    numGuests: 0,
+  };
+
+  const rest2 = {
+    name: "La Piazza",
+    owner: "Giovanni Rossi",
+  };
+
+  // OR assignment operator
+  rest1.numGuests = rest1.numGuests || 10;
+  rest2.numGuests = rest2.numGuests || 10;
+  rest1.numGuests ||= 10;
+  rest2.numGuests ||= 10;
+
+  // nullish assignment operator (null or undefined)
+  rest1.numGuests ??= 10;
+  rest2.numGuests ??= 10;
+
+  console.log(rest1);
+  console.log(rest2);
+
+  // Nullish: null and undefined (NOT 0 or '')
+  const guestCorrect = restaurant.numGuests ?? 10;
+  console.log(guestCorrect);
+
+  // Short Circuiting (&& and ||)
+  console.log("--- OR ---");
+  // Use ANY data type, return ANY data type, short-circuiting
+  console.log(3 || "Jonas");
+  console.log("" || "Jonas");
+  console.log(true || 0);
+  console.log(undefined || null);
+
+  console.log(undefined || 0 || "" || "Hello" || 23 || null);
+
+  restaurant.numGuests = 0;
+  const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+  console.log(guests1);
+
+  const guests2 = restaurant.numGuests || 10;
+  console.log(guests2);
+
+  console.log("--- AND ---");
+  console.log(0 && "Jonas");
+  console.log(7 && "Jonas");
+
+  console.log("Hello" && 23 && null && "jonas");
+  restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
 })();
