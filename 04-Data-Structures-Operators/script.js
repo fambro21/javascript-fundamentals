86052e437bb28fe55e35f5ab036a8d211a8a2cfb
jonathan_fambro <startek591@gmail.com>
2026-06-26 11:41:11 -0400

    feat:add examples of lopping objects

--- 04-Data-Structures-Operators/script.js
+++ 04-Data-Structures-Operators/script.js
@@ -302,4 +302,25 @@
   console.log(ordersSet);
 
   for (const order of ordersSet) console.log(order);
+
+  // Looping Objects: Object Keys, Values and Entries
+
+  // Property Names
+  const properties = Object.keys(openingHours);
+  console.log(properties);
+
+  let openStr = `We are open on ${properties.length} days:`;
+  for (const day of properties) {
+    openStr += `${day}, `;
+  }
+  console.log(openStr);
+
+  // Entire Object
+  const entries = Object.entries(openingHours);
+  console.log(entries);
+
+  // [key, value]
+  for (const [day, { open, close }] of entries) {
+    console.log(`On ${day} we open at ${open} and close at ${close}`);
+  }
 })();
